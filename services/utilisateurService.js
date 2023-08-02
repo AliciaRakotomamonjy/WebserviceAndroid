bb60062e449d257e87f3768f4898feed30da614d
laetitiarabibizaka <laetitiarabibizaka@gmail.com>
2023-08-02 08:13:37 +0300

    [Service] - create function doInscription in utilisateurService

---
+++ services/utilisateurService.js
@@ -0,0 +1,31 @@
+const Utilisateur = require('../models/Utilisateur');
+const bcrypt = require('bcrypt');
+
+
+async function doInscription(nom, prenom, dateNaissance, adresse, login, motdepasse) {
+  try {
+    const utilisateurExistant = await Utilisateur.findOne({ login });
+    if (utilisateurExistant) {
+      throw new Error('Cet utilisateur existe déjà.');
+    }
+
+    const motdepasseHache = await bcrypt.hash(motdepasse, 10);
+
+    const nouvelUtilisateur = new Utilisateur({
+      nom,
+      prenom,
+      dateNaissance,
+      adresse,
+      login,
+      motdepasse: motdepasseHache,
+    });
+
+    const utilisateurEnregistre = await nouvelUtilisateur.save();
+
+    return utilisateurEnregistre;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = { doInscription };
