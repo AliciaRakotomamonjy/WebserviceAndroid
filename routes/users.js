36407b8c26910568f5eae2c661f62a1ab120893e
laetitiarabibizaka <laetitiarabibizaka@gmail.com>
2023-08-02 08:14:22 +0300

    [Route] - add route for sign up

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const utilisateurService = require('../services/utilisateurService');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,6 +10,25 @@ router.get('/', function(req, res, next) {
     data: [],
     message: ""
   })
+});
+
+
+router.post('/signup', async (req, res) => {
+  const { nom, prenom, dateNaissance, adresse, login, motdepasse } = req.body;
+
+  try {
+    const nouvelUtilisateur = await utilisateurService.doInscription(
+      nom,
+      prenom,
+      dateNaissance,
+      adresse,
+      login,
+      motdepasse
+    );
+    res.status(201).json(nouvelUtilisateur);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 module.exports = router;
