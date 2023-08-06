965c1e03d08cef381b017d28a22793b5654f4ff7
Alicia <aliciarakotomamonjy@gmail.com>
2023-08-06 15:59:38 +0300

    [Service] :  insert Avis

--- services/avisService.js
+++ services/avisService.js
@@ -1,10 +1,18 @@
 const Avis = require('../models/Avis')
+const Article = require('../models/Article')
 
-const createAvis = async (data) => {
+const createAvis = async (data, idArticle) => {
     try {
         const newAvis = new Avis(data);
         inserted = await Avis.create(newAvis);
         console.log("New Avis : ", inserted);
+        Article.findByIdAndUpdate(idArticle, { $push: { avis: inserted._id } }, { new: true })
+            .then(updatedArticle => {
+                console.log('Article mis à jour :', updatedArticle);
+            })
+            .catch(err => {
+                console.error('Erreur lors de la mise à jour de l\'article :', err);
+            });
     } catch (error) {
         console.log(error);
         throw error;
